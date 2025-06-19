ee78270d8633e2ce28b72e45e7584169db6a9349
oknaim <omree.naim@liverpool.ac.uk>
2025-06-19 10:49:28 +0100

    Clarifying a commit.

--- R/create-plot.R
+++ R/create-plot.R
@@ -7,7 +7,7 @@ library("tidyverse")
 df = read_csv("data/weatherAUS.csv")
 
 # Create a scatter plot of Humidity9am and Temp9am
-#Specify a location
+# Specify a location with a city name
 
 city = "Brisbane"
 
