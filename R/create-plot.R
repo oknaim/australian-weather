601e6cd1d3df726373286040c1cd2945f6aff49e
oknaim <omree.naim@liverpool.ac.uk>
2025-06-19 11:09:11 +0100

    Altering the city from Brisbane to Sydney.

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,7 +9,7 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # Specify a location with a city name
 
-city = "Brisbane"
+city = "Sydney"
 
 scatter_plot = 
 df %>%
